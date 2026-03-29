230ba93e8520d01a4f376d2f492215b7789a5f63
Lucas Rocha <lucas_santos239@outlook.com>
2026-03-29 16:53:59 -0300

    Organization lab and update README

--- labs-praticos/main.tf
+++ labs-praticos/main.tf
@@ -1,6 +1,19 @@
+provider "aws" {
+  access_key = "test"
+  secret_key = "test"
+  region     = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    ec2 = "http://localhost:4566"
+    s3  = "http://localhost:4566"
+    iam = "http://localhost:4566"
+  }
+}
 
   # VARIÁVEIS: A planilha de dados da nossa infraestrutura
-  # ---------------------------------------------------------------------
 variable "subnet_prefix" {
   description = "Lista de objetos contendo os IPs e os nomes das sub-redes"
   
