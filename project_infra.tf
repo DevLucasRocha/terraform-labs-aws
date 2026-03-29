939ae6ed080ae369c1c87c342c48d0245ed49fe3
Lucas Rocha <lucas_santos239@outlook.com>
2026-03-29 16:34:19 -0300

    update files

--- project_infra.tf
+++ project_infra.tf
@@ -1,19 +1,3 @@
-provider "aws" {
-  # Simulação do provedor AWS com LocalStack
-  access_key                  = "test"
-  secret_key                  = "test"
-  region                      = "us-east-1"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    ec2 = "http://localhost:4566"
-    s3  = "http://localhost:4566"
-    iam = "http://localhost:4566"
-  }
-}
-
 # ---------------------------------------------------------------------
 # VARIÁVEIS: A planilha de dados da nossa infraestrutura
 # ---------------------------------------------------------------------
