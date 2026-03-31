25e9d8024bd0e0f0c6e53d92dafd56cfa4a4209b
DevLucasRocha <lucas_santos239@outlook.com>
2026-03-31 17:17:35 -0300

    feat: adiciona lab docker com servidor nginx web customizado

--- lab-docker/main.tf
+++ lab-docker/main.tf
@@ -7,10 +7,15 @@ resource "docker_image" "nginx" {
 # Passo 2: Rodar o contêiner usando a imagem baixada
 resource "docker_container" "nginx_server" {
   image = docker_image.nginx.image_id
-  name  = "meu-nginx-terraform"
+  name  = "nginx-terraform"
   
   ports {
     internal = 80    # A porta padrão do Nginx lá dentro
     external = 8080  # A porta que vamos acessar no seu navegador
+  }
+  # Injetando o site customizado para dentro do Nginx
+  volumes {
+    host_path      = abspath(path.cwd)
+    container_path = "/usr/share/nginx/html"
   }
 }
